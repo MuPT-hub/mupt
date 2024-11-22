8a04699f035fc6cc1fc4effca540df79a2a6521d
Timotej Bernat <tibe3324@colorado.edu>
2024-11-21 19:37:23 -0700

    Included real-valued generic

--- mupt/mutils/arraytypes.py
+++ mupt/mutils/arraytypes.py
@@ -10,10 +10,14 @@ T = TypeVar('T') # pure generics
 
 import numpy as np
 import numpy.typing as npt
-from numbers import Number
+from numbers import Number, Real
 
 
+# Numeric typehints
 Numeric = TypeVar('Numeric', bound=Number) # typehint a number-like generic type
+RealValued = TypeVar('RealValued', bound=Real)
+
+# Numpy array type annotations
 Shape = tuple # the shape field of a numpy array
 DType = TypeVar('DType', bound=np.generic) # the data type of a numpy array
 
@@ -22,7 +26,8 @@ M = TypeVar('M', bound=int) # typehint the size of a given dimension
 N = TypeVar('N', bound=int) # typehint the size of a given dimension
 P = TypeVar('P', bound=int) # typehint the size of a given dimension
 
-# this type of hard-coding kind sucks, but is he best we can do with the current Python type system
+# Fixed-size vector and array type annotations
+## DEV: this type of hard-coding kind sucks, but is the best we can do with the current Python type system
 Vector2  = Annotated[npt.NDArray[DType], Shape[2]]
 Array2x2 = Annotated[npt.NDArray[DType], Shape[2, 2]]
 ArrayNx2 = Annotated[npt.NDArray[DType], Shape[N, 2]]
