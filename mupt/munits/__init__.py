b189d7a479e4c9a7c0c74578ab84e29d20ac09bf
Timotej Bernat <tibe3324@colorado.edu>
2024-11-21 19:56:47 -0700

    Flagged unit dependency resolution for later implementation

--- mupt/munits/__init__.py
+++ mupt/munits/__init__.py
@@ -1,4 +1,4 @@
 '''Subpackage for determining how units and quantities are implemented internally'''
 
-# resolve between pint, OpenMM, OpenFF units (which is just a subflavor of pint) or toehr engines out there
+# resolve between pint, OpenMM, OpenFF units (which is just a subflavor of pint) or other engines out there
 # will partially depend on what units are available
