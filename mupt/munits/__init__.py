22baaab08727a4d4fcff3a46adeb7f69d9791b7b
Timotej Bernat <tibe3324@colorado.edu>
2024-11-21 19:22:41 -0700

    Added subpackage responsible for setting unit and quantity implementation

---
+++ mupt/munits/__init__.py
@@ -0,0 +1,4 @@
+'''Subpackage for determining how units and quantities are implemented internally'''
+
+# resolve between pint, OpenMM, OpenFF units (which is just a subflavor of pint) or toehr engines out there
+# will partially depend on what units are available
