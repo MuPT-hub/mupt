af60511dce52c5b061702865851d6e7263729642
Timotej Bernat <tibe3324@colorado.edu>
2024-11-21 19:36:53 -0700

    Added placeholder classes for Coordinates arrays and Conformers

--- mupt/mupr/geometry/coordinates.py
+++ mupt/mupr/geometry/coordinates.py
@@ -1,1 +1,20 @@
-'''For representing coordinate arrays and transformations, either for geometric objects or molecular conformers'''
+'''For representing coordinate arrays and transformations'''
+
+from typing import Generic, TypeVar
+from numbers import Number, Real
+
+Numeric = TypeVar('Numeric', bound=Number)
+
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+ 
+class Coordinates(Generic[Numeric]):
+    '''Encapsulation class for storing sets of coordinates and performing transfomations on those coordinates'''
+    pass
+
+class Conformer(Coordinates[Real]):
+    '''For representing the positions of units of a molecules'''
+    pass
+
+    # incorporate binding to atoms/masses 
