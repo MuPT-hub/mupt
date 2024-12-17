0ea6aae9553e8c0b4dbd80f2a3b2888d71be013a
Timotej Bernat <tibe3324@colorado.edu>
2024-12-17 13:43:48 -0700

    Added generic class for representing planes in 3-space

--- mupt/mupr/geometry/shapes.py
+++ mupt/mupr/geometry/shapes.py
@@ -3,7 +3,7 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
-from typing import Generic, TypeVar
+from typing import Generic, Sequence, TypeVar
 
 T = TypeVar('T')
 
@@ -13,7 +13,52 @@ from dataclasses import dataclass
 import numpy as np
 
 from ...mutils.decorators.classmod import register_abstract_class_attrs
-from ...mutils.arraytypes import Shape, DType, M, N, Dims, Vector3, ArrayNxN
+from ...mutils.arraytypes import Shape, Numeric, DType, M, N, Dims, Vector3, ArrayNxN
+
+
+@dataclass
+class Plane(Generic[Numeric]):
+    '''
+    Represents a plane in 3-space
+    Represents the locus of points (x, y, z) satisfying a*x + b*y + c*z + d = 0
+    '''
+    a : Numeric
+    b : Numeric
+    c : Numeric
+    d : Numeric = 0
+    
+    @classmethod
+    def from_normal_and_point(cls, normal : np.ndarray[Numeric, Shape[3,]], point : np.ndarray[Numeric, Shape[3,]]) -> 'Plane':
+        '''Initialize from a normal vector and an arbitrary point know to lie in the plane'''
+        assert isinstance(point, np.ndarray) and point.size == 3
+        a, b, c = normal
+        
+        assert isinstance(point, np.ndarray) and point.size == 3
+        d = np.dot(normal, point)
+        
+        return cls(a, b, c, -d)
+    
+    @property
+    def normal(self) -> np.ndarray[Numeric, Shape[3,]]:
+        return np.array([self.a, self.b, self.c])
+    
+    def contains(self, *point : Sequence[Numeric]) -> bool:
+        '''Test whether a point lies on the plane defined'''
+        if len(point) == 1 and isinstance(point[0], (Sequence, np.ndarray)):
+            point = point[0] # correct missing star-args for Sequence-like
+        
+        assert len(point) == 3
+        x, y, z = point
+        
+        return np.isclose(self.a*x + self.b*y + self.c*z + self.d, 0.0).astype(object) # convert from Numpy to Python bool
+    
+    def sample(self, radius : Numeric=1.0, n : int=1) -> np.ndarray[Numeric, Shape[3,]]:
+        '''Sample a random point from the plane within a given distance from the origin in the XY-plane (default 1 unit)'''
+        x = np.random.uniform(-radius, radius, size=n)
+        y = np.random.uniform(-radius, radius, size=n)
+        z = - (self.a*x + self.b*y + self.d)/(self.c) # z in constrained by first 2 choices
+        
+        return np.column_stack([x, y, z])
 
 
 @register_abstract_class_attrs('dimension') # requires that subclasses implement a dimensionality at the class level
