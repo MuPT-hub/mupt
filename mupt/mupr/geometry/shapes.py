2ab6088f52238f53766a78bd6bee467cc9d63416
Timotej Bernat <tibe3324@colorado.edu>
2025-04-07 22:34:00 -0600

    Provided implement for PointCloud subclass of BoundedShape

--- mupt/mupr/geometry/shapes.py
+++ mupt/mupr/geometry/shapes.py
@@ -8,12 +8,16 @@ from typing import Generic, Sequence, TypeVar
 T = TypeVar('T')
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import numpy as np
+from scipy.spatial import ConvexHull, Delaunay
 
+from .affine import apply_affine_transform_to_points
 from ...mutils.decorators.classmod import register_abstract_class_attrs
 from ...mutils.arraytypes import Shape, Numeric, DType, M, N, Dims, Vector3, ArrayNxN
+
+type Point3D = np.ndarray[Shape[3], Numeric]
 
 
 @dataclass
@@ -28,7 +32,7 @@ class Plane(Generic[Numeric]):
     d : Numeric = 0
     
     @classmethod
-    def from_normal_and_point(cls, normal : np.ndarray[Numeric, Shape[3,]], point : np.ndarray[Numeric, Shape[3,]]) -> 'Plane':
+    def from_normal_and_point(cls, normal : Point3D, point : Point3D) -> 'Plane':
         '''Initialize from a normal vector and an arbitrary point know to lie in the plane'''
         assert isinstance(point, np.ndarray) and point.size == 3
         a, b, c = normal
@@ -39,7 +43,7 @@ class Plane(Generic[Numeric]):
         return cls(a, b, c, -d)
     
     @property
-    def normal(self) -> np.ndarray[Numeric, Shape[3,]]:
+    def normal(self) -> Point3D:
         return np.array([self.a, self.b, self.c])
     
     def contains(self, *point : Sequence[Numeric]) -> bool:
@@ -52,7 +56,7 @@ class Plane(Generic[Numeric]):
         
         return np.isclose(self.a*x + self.b*y + self.c*z + self.d, 0.0).astype(object) # convert from Numpy to Python bool
     
-    def sample(self, radius : Numeric=1.0, n : int=1) -> np.ndarray[Numeric, Shape[3,]]:
+    def sample(self, radius : Numeric=1.0, n : int=1) -> Point3D:
         '''Sample a random point from the plane within a given distance from the origin in the XY-plane (default 1 unit)'''
         x = np.random.uniform(-radius, radius, size=n)
         y = np.random.uniform(-radius, radius, size=n)
@@ -63,7 +67,7 @@ class Plane(Generic[Numeric]):
 
 @register_abstract_class_attrs('dimension') # requires that subclasses implement a dimensionality at the class level
 class BoundedShape(ABC, Generic[T]): # template for numeric type (some iterations of float in most cases)
-    '''Interface for bounded, rigid bodies'''
+    '''Interface for bounded rigid bodies which can undergo coordinate transforms'''
     @property
     @abstractmethod
     def centroid(self) -> np.ndarray[Shape[Dims], T]:
@@ -95,7 +99,10 @@ class BoundedShape(ABC, Generic[T]): # template for numeric type (some iteration
         assert(affine_matrix.shape == (self.dimension + 1, self.dimension + 1)) # enforce squareness and dimensionality of transform
         return self._apply_affine_transformation(affine_matrix)
      
-     # TODO : require definition of support function?
+    # @abstractmethod
+    # def support(self, direction : np.ndarray[Shape[Dims], T]) -> np.ndarray[Shape[Dims], T]:
+    #     '''Determines the furthest point on the surface of the body in a given direction'''
+    #     ...
 
 # Concrete BoundedShape implementations
 @dataclass
@@ -116,12 +123,38 @@ class Sphere(BoundedShape[float], dimension=3):
         return bool(np.linalg.norm(self.center - point) < self.radius) # need to cast from numpy bool to Python bool
     
     def _apply_affine_transformation(self, affine_matrix : np.ndarray[Shape[4, 4], T]) -> 'Sphere':
-        affine_center = np.concatenate([self.center, [1.0]]) # center point in homogeneous coordinates
-        transformed_center_homog = affine_matrix @ affine_center
-        transformed_center = transformed_center_homog[:-1] / transformed_center_homog[-1] # project back down from homogeneous coordinates
-        
         return Sphere( # TODO: should return an ellipsoid if scaling in anisotropic
             radius=self.radius * np.linalg.det(affine_matrix)**(1/3), # scale radius appropriately
-            center=transformed_center,
-        )
+            center=apply_affine_transform_to_points(self.center, affine_matrix),
+        ) # TODO: have non-isometric affine transforms correctly return an Ellipsoid, once implemented
 
+# TODO: implement Ellipsoid subclass
+
+@dataclass
+class PointCloud(BoundedShape[float], dimension=3):
+    '''A cluster of points in 3D space'''
+    coordinates : np.ndarray[Shape[3], float]
+    convex_hull : ConvexHull = field(init=False)
+    triangulation : Delaunay = field(init=False)
+    
+    def __post_init__(self) -> None:
+        # TODO: validate coordinates
+        self.convex_hull = ConvexHull(self.coordinates)
+        self.triangulation = Delaunay(self.convex_hull)
+    
+    @property
+    def centroid(self) -> np.ndarray[Shape[3], float]:
+        return self.coordinates.mean(axis=0)
+    
+    @property
+    def volume(self) -> float:
+        return self.convex_hull.volume
+    
+    def contains(self, point : np.ndarray[Shape[3], float]) -> bool:   # TODO: decide whether containment should be boundary-inclusive
+        return bool(self.triangulation.find_simplex(point) != -1) # need to cast from numpy bool to Python bool
+    
+    def _apply_affine_transformation(self, affine_matrix : np.ndarray[Shape[4, 4], T]) -> 'PointCloud':
+        return PointCloud(
+            coordinates=apply_affine_transform_to_points(self.coordinates, affine_matrix)
+        )
+    
