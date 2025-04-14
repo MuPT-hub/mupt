01f0668a547bc285e03f897ffd9128e59577721f
Timotej Bernat <tibe3324@colorado.edu>
2025-04-14 16:45:50 -0600

    Added new transforms "geometry" submodule

---
+++ mupt/geometry/transforms.py
@@ -0,0 +1,36 @@
+'''Utilities for computing and applying common linear and affine transformations as matrices'''
+
+from typing import Optional, TypeVar
+T = TypeVar('T')
+
+import numpy as np
+from .arraytypes import Shape, Dims
+
+
+def parallel_projector(direction_vector : np.ndarray[Shape[Dims], T]) -> np.ndarray[Shape[Dims, Dims], T]:
+    '''Computes a linear transformation which, when applied to an arbitrary vector,
+    yields the component of that vector parallel to the direction vector provided
+    
+    Returns matrix which represents the parallel projector transformation'''
+    (dim,) = direction_vector.shape # extract dimension of vector space (and implicitly enforce 1D array)
+    return np.outer(direction_vector, direction_vector) / np.inner(direction_vector, direction_vector)
+
+def orthogonal_projector(direction_vector : np.ndarray[Shape[Dims], T]) -> np.ndarray[Shape[Dims, Dims], T]:
+    '''Computes a linear transformation which, when applied to an arbitrary vector,
+    yields the component of that vector orthogonal to the direction vector provided
+    
+    Returns matrix which represents the orthogonal projector transformation'''
+    (dim,) = direction_vector.shape # extract dimension of vector space (and implicitly enforce 1D array)
+    return np.eye(dim, dtype=direction_vector.dtype) - parallel_projector(direction_vector) # equivalent to substracting parallel part off of vector transform is applied to
+perpendicular_projector = orthogonal_projector # alias for convenience
+
+def alignment_transform(initial_vector : np.ndarray[Shape[Dims], T], final_vector : np.ndarray[Shape[Dims], T]) -> np.ndarray[Shape[Dims, Dims], T]:
+    '''Compute an orthogonal linear transformation which takes a given initial vector to a final vector
+    while preserving the relative orientations of the basis vectors to one another
+    
+    Returns an orthogonal Householder matrix which represents the transformation'''
+    (dim,) = initial_vector.shape # extract dimension of vector space (and implicitly enforce 1D array)
+    assert final_vector.shape == initial_vector.shape # ensure both vectors have 
+    diff = final_vector - initial_vector # could arbitrarily reverse order, will only differ in sign of reflection plane normal
+    
+    return np.eye(dim, dtype=initial_vector.dtype) - 2*parallel_projector(diff) # compute Householder reflection
