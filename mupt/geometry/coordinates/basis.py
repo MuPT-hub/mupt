07c539c428d5fae88df1586475072c055b8ec327
Timotej Bernat <tibe3324@colorado.edu>
2025-04-22 21:00:17 -0600

    Added methods to check for mutual orthogonality of row an column bases

--- mupt/geometry/coordinates/basis.py
+++ mupt/geometry/coordinates/basis.py
@@ -7,15 +7,30 @@ import numpy as np
 from ..arraytypes import Shape, N, Dims, Numeric
 
 
+def is_diagonal(matrix : np.ndarray[Shape[N, N], Numeric]) -> bool: # TODO: generalize to work for other diagonals
+    '''Determine whether a matrix is digonal, i.e. has no nonzero elements off of the main diagonal'''
+    i, j = np.nonzero(matrix)
+    return (i == j) # check that no nonzero entries lies off the diagonal
+
+def is_rowspace_mutually_orthogonal(matrix : np.ndarray[Shape[N, N], Numeric]) -> bool:
+    '''Check whether all vectors in the row space basis of a matrix are mutually orthogonal'''
+    return is_diagonal(matrix @ matrix.T) # note CAREFULLY the order; P_ij = dot(row(i), row(j)) this way
+
+def is_columnspace_mutually_orthogonal(matrix : np.ndarray[Shape[N, N], Numeric]) -> bool:
+    '''Check whether all vectors in the column space basis of a matrix are mutually orthogonal'''
+    return is_diagonal(matrix.T @ matrix) # note CAREFULLY the order; P_ij = dot(column(i), column(j)) this way
+
 def is_orthogonal(matrix : np.ndarray[Shape[N, N], Numeric]) -> bool:
     '''
     Determine if a matrix is orthogonal, i.e. its left and right inverses are both its own transpose
-    Note that the matrix does not necessailry have to be square in order for it to be orthogonal
+    Note that the matrix does not necessarily have to be square in order for it to be orthogonal
     '''
     (n_rows, n_cols) = matrix.shape # implicitly assert 2-dimensionality
+    prod = matrix @ matrix.T
     
     return  np.allclose(matrix @ matrix.T, np.eye(n_rows, dtype=matrix.dtype)) \
-        and np.allclose(matrix.T @ matrix, np.eye(n_cols, dtype=matrix.dtype)) 
+        and np.allclose(matrix.T @ matrix, np.eye(n_cols, dtype=matrix.dtype)) # NOTE: can't optimize as the transpose of the above product for non-square matrices
+is_orthonormal = is_orthogonal
 
 def compute_local_coordinates(positions : np.ndarray[Shape[N, Dims], float]) -> tuple[
         np.ndarray[Shape[Dims], float],
