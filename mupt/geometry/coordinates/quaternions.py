a4b52c49ec058ee68cd2bae007111f73b629eb9b
Timotej Bernat <tibe3324@colorado.edu>
2025-04-22 18:32:10 -0600

    Added placeholder submodule for quaternion operations

---
+++ mupt/geometry/coordinates/quaternions.py
@@ -0,0 +1,4 @@
+'''Transformation and conversion operations on Hamiltonian quaterions'''
+
+from scipy.spatial.transform import Rotation
+# import rowan ## TOSELF: worth employing eventually for HOOMD compat?
