0d3641f6464226ac342bac5a3be58c073d9a9eb4
Timotej Bernat <tibe3324@colorado.edu>
2025-04-24 21:16:59 -0600

    Fixed indefinite array size typehints, removed unnecessary imports

--- mupt/tests/geometry/test_homogeneous.py
+++ mupt/tests/geometry/test_homogeneous.py
@@ -4,13 +4,12 @@ __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
 import pytest
+from typing import Any
 import numpy as np
 
 from mupt.geometry.coordinates.homogeneous import (
     to_homogeneous_coords,
     from_homogeneous_coords,
-    affine_matrix_from_linear_and_center,
-    apply_affine_transform_to_points,
 )
 from mupt.geometry.arraytypes import Shape, N, M, Dims, DimsPlus, Numeric
 
@@ -21,7 +20,7 @@ vector = np.random.random((N, 3))
 block  = np.random.random((N, N, 3))
 
 @pytest.mark.parametrize('array', (point, vector, block,))
-def test_to_homogeneous_coords(array : np.ndarray[Shape[..., 3], Numeric]) -> None:
+def test_to_homogeneous_coords(array : np.ndarray[Shape[Any, 3], Numeric]) -> None:
     '''Test the conversion of arbitrarily-nested arrays of 3D coordinates to homogeneous coordinates'''
     projection : int = 1.0
     *array_shape, xyz_shape = array.shape
@@ -31,7 +30,7 @@ def test_to_homogeneous_coords(array : np.ndarray[Shape[..., 3], Numeric]) -> No
     assert (homog.shape == expected_shape) and np.allclose(homog[..., -1], projection)
     
 @pytest.mark.parametrize('array', (point, vector, block,))
-def test_from_homogeneous_coords(array : np.ndarray[Shape[..., 3], Numeric]) -> None:
+def test_from_homogeneous_coords(array : np.ndarray[Shape[Any, 3], Numeric]) -> None:
     '''Test that conversion to and back from homogeneous coordinates has no side effects'''
     piped_array = from_homogeneous_coords(to_homogeneous_coords(array))
     assert np.allclose(piped_array, array)
