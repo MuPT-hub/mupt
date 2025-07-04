59b6c60745ae11c0eb596072b54bdf9aade51a5d
Timotej Bernat <tibe3324@colorado.edu>
2025-07-03 19:09:54 -0600

    Deprecated in-house quaternions module in favor of scipy's handling of quaternions

--- mupt/geometry/coordinates/quaternions.py
+++
@@ -1,4 +0,0 @@
-'''Transformation and conversion operations on Hamiltonian quaterions'''
-
-from scipy.spatial.transform import Rotation
-# import rowan ## TOSELF: worth employing eventually for HOOMD compat?
