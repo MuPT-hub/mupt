f94d8aa720ca62af1d2140c1ef2630895285f3ba
Timotej Bernat <tibe3324@colorado.edu>
2025-07-08 20:03:32 -0600

    Expanded transforms.rigid into subpackage

---
+++ mupt/geometry/transforms/rigid/__init__.py
@@ -0,0 +1,7 @@
+'''Utilities for creating rigid transformations and applying them to points in 3D space
+i.e. for working with the Special Euclidean isometry group SE(3)'''
+
+__author__ = 'Timotej Bernat'
+__email__ = 'timotej.bernat@colorado.edu'
+
+from .application import apply_rigid_transformation_recursive, RigidTransformable
