911c0dcd205519c9b72d063f25c8859b40cbc3c3
Timotej Bernat <tibe3324@colorado.edu>
2025-07-24 18:12:25 -0600

    Added missing __init__ to mupr.topology

---
+++ mupt/mupr/topology/__init__.py
@@ -0,0 +1,4 @@
+'''Encodings of graphs which represent the topology and connectivity of abstract primitives in a polymer system'''
+
+__author__ = 'Timotej Bernat'
+__email__ = 'timotej.bernat@colorado.edu'
