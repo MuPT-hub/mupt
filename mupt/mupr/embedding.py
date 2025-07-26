d7b617fdf25443430af85c8cbc279272b0796bc4
Timotej Bernat <tibe3324@colorado.edu>
2025-07-25 19:42:10 -0600

    Added embeddings module for inserting Primitives into graphs to recursively build structure

---
+++ mupt/mupr/embedding.py
@@ -0,0 +1,7 @@
+'''Utilities for inserting Primitives into pre-built graphs to create polymer topology graphs'''
+
+__author__ = 'Timotej Bernat'
+__email__ = 'timotej.bernat@colorado.edu'
+
+# DEVNOTE: this is not a submodule under topology to avoid circular imports
+# and to shelter MID Graphs from needing to know about HOW they're embedded
