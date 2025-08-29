834e51e7c3beced335ab57c2194a999d17a687eb
Timotej Bernat <tibe3324@colorado.edu>
2025-08-28 18:17:11 -0600

    Made copyable Protocol runtime-checkable

--- mupt/mutils/copyable.py
+++ mupt/mutils/copyable.py
@@ -3,9 +3,10 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
-from typing import Protocol, Self
+from typing import Protocol, Self, runtime_checkable
 
 
+@runtime_checkable
 class Copyable(Protocol):
     '''Any class which supports creating a copy of instances of the class'''
     def copy(self) -> Self:
