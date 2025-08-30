8eb6929619fffa35fe4310572e8047c2c76a8e5c
Timotej Bernat <tibe3324@colorado.edu>
2025-08-29 18:46:38 -0600

    Added improved RigidlyTransformable Protocol which handles boilerplate transformation provenance and out-of-place application

--- mupt/geometry/transforms/rigid/application.py
+++ mupt/geometry/transforms/rigid/application.py
@@ -3,19 +3,81 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
-from typing import Any, Mapping, Sequence, Union
+from typing import Any, Mapping, Self, Sequence, Union
 from typing import Protocol, runtime_checkable
+from abc import ABC, abstractmethod
 
 from scipy.spatial.transform import RigidTransform
 
+from ....mutils.copyable import Copyable, NotCopyableError
+
+
+@runtime_checkable
+class RigidlyTransformable(Protocol):
+    '''Mixin for objects which support rigid transformations'''
+    # DEV: went back and forth on verbiage, but settled on the following as least ambiguous:
+    # * "transformation" to refer to the RigidTransforms passed around
+    # * "transform" to refer to the act of applying a transformation (NOT the transformation itself)
+    # * "transformed" to indicate that a transform was applied to a COPY of an object
+    # DON'T change these names until you've understood this and made similar considerations for proposed changes
+
+    # transform provenance
+    @property
+    def cumulative_transformation(self) -> RigidTransform:
+        '''
+        The net transformation applied to this object since it was instantiated
+        Heuristically, this describes "how much the object has been moved from where it started"
+
+        Used for tracking provenance of rigid transformations and handing that information off between objects
+        '''
+        if not hasattr(self, '_cumul_transf'): # DEV: deliberately mangled name to not resemble it "public" counterpart much
+            self._cumul_transf = RigidTransform.identity()
+        return self._cumul_transf
+    
+    @cumulative_transformation.setter
+    def cumulative_transformation(self, transformation : RigidTransform) -> None:
+        # DEV: might include some additional checks in here in the future
+        self._cumul_transf = transformation
+        
+    @property
+    def resetting_transformation(self) -> RigidTransform:
+        '''The transformation that resets the object to its original configuration'''
+        return self.cumulative_transformation.inv()
+
+    # application of transformations
+    @abstractmethod
+    def _rigidly_transform(self, transformation : RigidTransform) -> None:
+        raise NotImplemented # implement subclass-specific behavior here
+        
+    def rigidly_transform(self, transformation : RigidTransform) -> None:
+        self._rigidly_transform(transformation)
+        self.cumulative_transformation = transformation * self.cumulative_transformation
+
+    def rigidly_transformed(self, transformation: RigidTransform) -> Self:
+        if not isinstance(self, Copyable):
+            raise NotCopyableError(f'Class "{self.__class__.__name__}" does not implement mechanism for copying objects')
+        
+        clone = self.copy()
+        clone.rigidly_transform(transformation)
+        
+        return clone
+    
+    def reset_transform(self) -> None:
+        '''Return the object to its un-transformed configuration'''
+        self.rigidly_transform(self.resetting_transformation)
+
+    def reset_transformed(self) -> Self:
+        '''Return an un-transformed copy of this object'''
+        return self.rigidly_transformed(self.resetting_transformation)
 
 @runtime_checkable
 class RigidTransformable(Protocol):
-    '''Interface for objects that can undergo a rigid (isometric) transformation'''
-    def apply_rigid_transformation(self, transformation: RigidTransform) -> Any: 
-        # DEVNOTE: regarding typehints, returned type may be different to type of self, and is not necessarily transformable either
+    '''Interface for objects that can undergo a rigid transformation'''
+    def apply_rigid_transformation(self, transformation: RigidTransform) -> 'RigidTransformable': 
+        # DEVNOTE: regarding typehints, returned type may be different to type of self
+        # but ought to still be transformable, since rigid transformations should, in general, be invertible
         ...
-
+        
 def apply_rigid_transformation_recursive(
         obj : Union[object, Sequence[Any], Mapping[str, Any]],
         transformation: RigidTransform,
--- mupt/mutils/copyable.py
+++ mupt/mutils/copyable.py
@@ -11,3 +11,7 @@ class Copyable(Protocol):
     '''Any class which supports creating a copy of instances of the class'''
     def copy(self) -> Self:
         ...
+
+class NotCopyableError(Exception):
+    '''Raised when a copy-based operation is invokes on an object whose class doesn't implement it'''
+    ...
