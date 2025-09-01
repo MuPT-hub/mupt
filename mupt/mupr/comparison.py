3911e6850d2104fad6e7b3e05c60aa458fc71721
Timotej Bernat <tibe3324@colorado.edu>
2025-09-01 17:29:38 -0600

    Revised types of comparability, deprecated equivalence classes function (by hash is trivial to implement w/ default dict, by relation is implemented in networkx)

--- mupt/mupr/comparison.py
+++ mupt/mupr/comparison.py
@@ -10,28 +10,21 @@ from collections import defaultdict
 
 class Comparable(Protocol):
     '''Objects which can be compared by spatial similarity (coincidence), structural similarity (congruence), or both'''
-    def coincident_with(self, other: Self) -> bool:
-        '''Occupying the same space as the other object'''
+    def coincides_with(self, other: Self) -> bool:
+        '''Whether the parts of this object are spatially identical to those of the other'''
         ...
         
+    def resembles(self, other: Self) -> bool: # DEV: resemblant_of()??
+        '''Whether the parts of this object are of the same types, without necessarily being coincident or identical objects'''
+        ...
+
     def congruent_to(self, other: Self) -> bool:
         '''Not necessarily coincident, but capable of becoming coincident (e.g. by orthogonal Procrustes)'''
         ...
-
-    def equivalent_to(self, other: Self) -> bool:
-        '''Having the same types of parts while not necessarily being spatially similar or identical objects'''
-        ...
         
-    def similar_to(self, other: Self) -> bool:
-        ...
-        
-    def analogous_to(self, other: Self) -> bool:
-        ...
+    # DEV: other terms to consider: "equivalent", "similar", "analogous", "fungible", ...
 
-def equivalence_classes(objects : Iterable[T], by_property : Callable[[T], Hashable]) -> set[frozenset[T]]:
-    '''Generate equivalence classes of objects of type T according to a binary relation'''
-    equiv_classes = defaultdict(set)
-    for obj in objects:
-        equiv_classes[by_property(obj)].add(obj)
-
-    return set(frozenset(equiv_class) for equiv_class in equiv_classes.values())
+    def fungible_with(self, other: Self) -> bool: # NOTE: opted not to use "interchangeable" to avoid confusion w/ OpenFF Interchange
+        '''Whether this object can be replaced by "other" without affecting the behavior or output of programs involving this object'''
+        return self.coincides_with(other) and self.resembles(other)
+    
