404d4a43ce34c7337a7de5a88b6c400192f4cb51
Timotej Bernat <tibe3324@colorado.edu>
2025-10-03 13:06:19 -0600

    Split smiles parsing into separate interfaces module from rdkit parsing

--- mupt/interfaces/smiles.py
+++ mupt/interfaces/smiles.py
@@ -2,3 +2,59 @@
 
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
+
+from typing import Hashable, Optional
+
+from rdkit.Chem.rdmolops import (
+    AddHs,
+    SanitizeMol,
+    SanitizeFlags,
+    SANITIZE_ALL,
+)
+from rdkit.Chem.rdmolfiles import (
+    MolFromSmiles,
+    MolToSmiles,
+    SmilesWriteParams,
+)
+from rdkit.Chem.rdDistGeom import EmbedMolecule
+
+from . import DEFAULT_SMILES_READ_PARAMS, DEFAULT_SMILES_WRITE_PARAMS
+from .rdkit import primitive_from_rdkit, primitive_to_rdkit
+from ..mupr.primitives import Primitive
+
+
+def primitive_from_smiles(
+    smiles : str, 
+    ensure_explicit_Hs : bool=True,
+    embed_positions : bool=False,
+    sanitize_ops : SanitizeFlags=SANITIZE_ALL,
+    label : Optional[Hashable]=None,
+    smiles_writer_params=DEFAULT_SMILES_WRITE_PARAMS,
+) -> Primitive:
+    '''Create a Primitive from a SMILES string, optionally embedding positions if selected'''
+    rdmol = MolFromSmiles(smiles, sanitize=False)
+    if ensure_explicit_Hs:
+        rdmol.UpdatePropertyCache() # allow Hs to be added without sanitizating twice
+        rdmol = AddHs(rdmol)
+    SanitizeMol(rdmol, sanitizeOps=sanitize_ops)
+    
+    conformer_id : Optional[int] = None
+    if embed_positions:
+        conformer_id = EmbedMolecule(rdmol, clearConfs=False) # NOTE: don't clobber existing conformers for safety (though new Mol shouldn't have any anyway)
+    
+    return primitive_from_rdkit(
+        rdmol,
+        conformer_id=conformer_id,
+        label=label,
+        smiles_writer_params=smiles_writer_params,
+    )
+
+def primitive_to_smiles(
+    primitive : Primitive,
+    smiles_params : Optional[SmilesWriteParams]=DEFAULT_SMILES_READ_PARAMS,
+) -> str:
+    '''Convert a Primitive to a SMILES string'''
+    return MolToSmiles(
+        primitive_to_rdkit(primitive),
+        params=smiles_params,
+    )
