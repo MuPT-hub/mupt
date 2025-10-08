9f9649cd45c5da2096736ca371af21fd95e6a85d
Timotej Bernat <tibe3324@colorado.edu>
2025-10-08 16:05:00 -0600

    Added sliding window and iterator length functions

--- mupt/mutils/iteration.py
+++ mupt/mutils/iteration.py
@@ -14,8 +14,18 @@ from typing import (
 )
 T = TypeVar('T')
 
-from itertools import count
+from collections import deque
+from itertools import count, islice
 
+
+def iter_len(itera : Iterable[T]) -> int:
+    '''
+    Get number of elements in an iterable object, even if unsized (namely a generator)
+    
+    Note that this will "use up" an iterator on call, i.e. 
+    DON'T call this on collections you intend to iterate over later
+    '''
+    return sum(1 for _  in itera)
 
 def ad_infinitum(value : Any) -> Generator[Any, None, None]:
     '''Wrap a single value in an inexhaustible generator which always returns that value'''
@@ -44,6 +54,19 @@ def flexible_iterator(
     else:
         raise TypeError(f'Singleton values converted to iterator must be an instance of {allowed_types}, not {type(values)}')
         
+def sliding_window(items : Iterable[T], n : int=1) -> Generator[tuple[T], None, None]:
+    '''Generates sliding windows of width n over an iterable collection of items
+    E.g. : sliding_window('ABCDE', 3) --> (A, B, C), (B, C, D), (C, D, E)
+    '''
+    it = iter(items)
+    window = deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+
+    for x in it: # implicit else
+        window.append(x) # owing to maxlen constraint, the first item in the is automatically discarded
+        yield tuple(window)        
+
 def int_complement(integers : Sequence[int], bounded : bool=False) -> Generator[int, None, None]:
     '''
     Given a sequence of integers, generates the complement of that sequence within the natural numbers,
