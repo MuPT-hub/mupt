c7a174f31731bd984fdb5d9e07b7df0f9dc90b79
Timotej Bernat <tibe3324@colorado.edu>
2025-10-17 19:32:33 -0600

    Added compilation of bond order reference by bond type

--- mupt/chemistry/core.py
+++ mupt/chemistry/core.py
@@ -3,8 +3,12 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
+import logging
+LOGGER = logging.getLogger(__name__)
+
 from typing import Union
 
+from rdkit.Chem.rdmolfiles import MolFromSmiles
 from rdkit.Chem.rdchem import Atom, BondType, GetPeriodicTable
 RDKitPeriodicTable = GetPeriodicTable()
 
@@ -13,6 +17,31 @@ from periodictable.core import Element, Ion, Isotope, isatom
 ELEMENTS = elements
 ElementLike = Union[Element, Ion, Isotope]
 
+from .rdloggers import suppress_rdkit_logs
+
+
+def _compile_bond_order_reference() -> dict[BondType, float]:
+    '''
+    Generate reference table of BondType to corresponding electronic bond order 
+    (e.g. aromatic = 1.5, double = 2, etc.), consistent with RDKit's definition
+    '''
+    dummy = MolFromSmiles('*-*')
+    bond = dummy.GetBondWithIdx(0) # DEV: can't directly initialize Bond from Python, so using this hacky aprroach to setup instead
+
+    bond_orders_by_bond_type : dict[BondType, float] = dict()
+    for bondtype in BondType.names.values():
+        bond.SetBondType(bondtype)
+        with suppress_rdkit_logs('rdApp.error'):
+            try:
+                # N.B.: these values are NOT the same as the keys of BondType.values; those are arbitrary indices, 
+                # whereas the bond order here conveys info loosely about the number of electrons per bond
+                bond_orders_by_bond_type[bondtype] = bond.GetBondTypeAsDouble()
+            except RuntimeError:
+                # DEV: functions as a warning, but want this to be suppressed nominally
+                LOGGER.debug(f'RDKit BondType {bondtype!s} does not have a double-valued bond order defined')
+
+    return bond_orders_by_bond_type
+BOND_ORDER : dict[BondType, float] = _compile_bond_order_reference()
 
 def valence_allowed(atomic_num : int, charge : int, valence : int) -> bool:
     '''Check if the given valence is allowed for the specified element'''
