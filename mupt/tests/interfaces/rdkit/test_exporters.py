881ff73c6e13b0f8ad6c0e5fb9fe0f4653e982d5
Timotej Bernat <tibe3324@colorado.edu>
2025-10-17 19:55:31 -0600

    Created placeholder module for RDKit exporter tests

---
+++ mupt/tests/interfaces/rdkit/test_exporters.py
@@ -0,0 +1,12 @@
+'''
+Tests for ensuring export from MuPT to RDKit preserves chemical information and metadata,
+and does not export systems which cannot be interpreted as all-atom molecules
+'''
+
+__author__ = 'Timotej Bernat'
+__email__ = 'timotej.bernat@colorado.edu'
+
+import pytest
+from mupt.interfaces.rdkit import exporters
+
+# TODO: test chemical info (e.g. charge, isotope, etc.) is preserved on atoms
