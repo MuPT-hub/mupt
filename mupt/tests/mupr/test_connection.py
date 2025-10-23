82fa784e14ff3d0f8c27a444878ec22927f86020
Timotej Bernat <tibe3324@colorado.edu>
2025-10-22 20:58:25 -0600

    Moved Connector bondability tests examples from mol_from_scratch example notebook to tests

--- mupt/tests/mupr/test_connection.py
+++ mupt/tests/mupr/test_connection.py
@@ -9,10 +9,19 @@ from mupt.chemistry.core import BondType
 from mupt.mupr.connection import Connector, AttachmentPoint, AttachmentLabel, TraversalDirection
 
 
+# bondability tests
+C1 = Connector(anchor=AttachmentPoint({'a'}), linker=AttachmentPoint({'z'}), bondtype=BondType.DOUBLE)
+C2 = Connector(anchor=AttachmentPoint({'b'}), linker=AttachmentPoint({'a'}), bondtype=BondType.SINGLE) # should fail, anchor of p1 not in linkables
+C3 = Connector(anchor=AttachmentPoint({'z'}), linker=AttachmentPoint({'a'}), bondtype=BondType.SINGLE) # should fail, bond types differ
+C4 = Connector(anchor=AttachmentPoint({'z'}), linker=AttachmentPoint({'a'}), bondtype=BondType.DOUBLE) # should not fail, since Connector pair is compatible
+
 @pytest.mark.parametrize(
     'conn1, conn2, expected_bondable', 
-    [ # TODO: populate with examples
-        # (Connector(), Connector(), True)
+    [
+        (Connector(), Connector(), False), # both empty - no way to have non-disjoint attachment point sets
+        (C1, C2, False), # anchorables of C1 not in linkables of C1
+        (C1, C3, False), # bond types differ
+        (C1, C4, True),  # compatible connectors
     ]
 )
 def test_connector_bondability(conn1 : Connector, conn2 : Connector, expected_bondable : bool) -> None:
