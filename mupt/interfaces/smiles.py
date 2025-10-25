7085a0f54ce1ceb6345c881d3460c8181509d501
Timotej Bernat <tibe3324@colorado.edu>
2025-10-24 18:38:24 -0600

    Fixed SMILES read/write default parameters, replaced bespoke sanitization with new sanitized_mol()

--- mupt/interfaces/smiles.py
+++ mupt/interfaces/smiles.py
@@ -6,10 +6,10 @@ __email__ = 'timotej.bernat@colorado.edu'
 from typing import Hashable, Optional
 
 from rdkit.Chem.rdmolops import (
-    AddHs,
-    SanitizeMol,
     SanitizeFlags,
     SANITIZE_ALL,
+    AromaticityModel,
+    AROMATICITY_MDL,
 )
 from rdkit.Chem.rdmolfiles import (
     MolFromSmiles,
@@ -21,23 +21,26 @@ from rdkit.Chem.rdDistGeom import EmbedMolecule
 from .rdkit import primitive_from_rdkit, primitive_to_rdkit
 from ..mupr.primitives import Primitive
 from ..chemistry.smiles import DEFAULT_SMILES_READ_PARAMS, DEFAULT_SMILES_WRITE_PARAMS
+from ..chemistry.sanitization import sanitized_mol
 
 
 def primitive_from_smiles(
     smiles : str, 
-    ensure_explicit_Hs : bool=True,
-    embed_positions : bool=False,
-    sanitize_ops : SanitizeFlags=SANITIZE_ALL,
     label : Optional[Hashable]=None,
-    smiles_writer_params=DEFAULT_SMILES_WRITE_PARAMS,
+    embed_positions : bool=False,
+    ensure_explicit_Hs : bool=True,
+    aromaticity_model : AromaticityModel=AROMATICITY_MDL,
+    sanitize_ops : SanitizeFlags=SANITIZE_ALL,
+    smiles_reader_params=DEFAULT_SMILES_READ_PARAMS,
+    smiles_writer_params=DEFAULT_SMILES_WRITE_PARAMS, 
 ) -> Primitive:
     '''Create a Primitive from a SMILES string, optionally embedding positions if selected'''
-    rdmol = MolFromSmiles(smiles, sanitize=False)
-    if ensure_explicit_Hs:
-        rdmol.UpdatePropertyCache() # allow Hs to be added without sanitizating twice
-        rdmol = AddHs(rdmol)
-    SanitizeMol(rdmol, sanitizeOps=sanitize_ops)
-    
+    rdmol = sanitized_mol(
+        MolFromSmiles(smiles, params=smiles_reader_params),
+        add_Hs=ensure_explicit_Hs,
+        sanitize_ops=sanitize_ops,
+        aromaticity_model=aromaticity_model,
+    )
     conformer_idx : Optional[int] = None
     if embed_positions:
         conformer_idx = EmbedMolecule(rdmol, clearConfs=False) # NOTE: don't clobber existing conformers for safety (though new Mol shouldn't have any anyway)
@@ -46,15 +49,15 @@ def primitive_from_smiles(
         rdmol,
         conformer_idx=conformer_idx,
         label=label,
-        smiles_writer_params=smiles_writer_params,
+        smiles_writer_params=smiles_writer_params, # DEV: needed to generate SMILES from mol in case no explicit label is provided
     )
 
 def primitive_to_smiles(
     primitive : Primitive,
-    smiles_params : Optional[SmilesWriteParams]=DEFAULT_SMILES_READ_PARAMS,
+    smiles_write_params : Optional[SmilesWriteParams]=DEFAULT_SMILES_WRITE_PARAMS,
 ) -> str:
     '''Convert a Primitive to a SMILES string'''
     return MolToSmiles(
         primitive_to_rdkit(primitive),
-        params=smiles_params,
+        params=smiles_write_params,
     )
