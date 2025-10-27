c2fab901b0515333e0af9df370c07eab1f6669c6
Eric Jankowski <ericjankowski@boisestate.edu>
2025-10-27 16:59:09 -0600

    framework for sliding window iteration

--- mupt/builders/dpd.py
+++ mupt/builders/dpd.py
@@ -79,6 +79,7 @@ class DPD_RandomWalk(PlacementGenerator):
     '''
     def __init__(
         self,
+        primitive,
         density=0.2,
         k=20000,
         bond_l=1.0,
@@ -89,6 +90,7 @@ class DPD_RandomWalk(PlacementGenerator):
         dt=0.001,
         particle_spacing=1.1
     ) -> None:
+        self.primitive = primitive
         self.density = density
         self.k = k
         self.bond_l = bond_l
@@ -105,6 +107,7 @@ class DPD_RandomWalk(PlacementGenerator):
         Returns the pair of node labels of the termini (a pair of the same value twice for single-node graphs)
         '''
         termini = tuple(chain.termini)
+        print(termini) #DEBUG
         if len(termini) == 2:
             return termini
         elif len(termini) == 1: 
@@ -113,50 +116,56 @@ class DPD_RandomWalk(PlacementGenerator):
             raise ValueError('Unbranched topology must have either 1 or 2 terminal nodes')
 
     # implementing builder contracts
-    def check_preconditions(self, primitive : Primitive) -> None:
+    def check_preconditions(self, primitive:Primitive) -> None:
         '''Enforce that no branches chains exist anywhere'''
         if primitive.topology.is_branched:
             raise ValueError('Random walk chain builder behavior undefined for branched topologies')
         
-        if any((subprim.shape is None) for subprim in primitive.children):
-            raise TypeError('Random walk chain builder requires ellipsoidal of spherical beads to determine step sizes')
+        #TODO: Add shapes
+        #if any((subprim.shape is None) for subprim in primitive.children):
+        #    raise TypeError('Random walk chain builder requires ellipsoidal or spherical beads to determine step sizes')
     
-
-
-
-    def _generate_placements(self, primitive : Primitive) :
+    def _generate_placements(self, primitive:Primitive) :
         '''
         Trying to use universe of chains to set monomer positions
         primitive passed in here should be a universe primitive that has chains to loop over 
         paths are lists of handles
         If we assume chains are looped over in the same way, we can map from handles to indices
         '''
-        #set up hoomd data structures to initialize simulation
         frame = gsd.hoomd.Frame()
         frame.particles.types = ['A'] #TODO: introduce HMT's?
-        frame.particles.N = primitive.topology.number_of_nodes() 
+        frame.particles.N = self.primitive.topology.number_of_nodes() 
         frame.particles.typeid = np.zeros(frame.particles.N)
         frame.particles.position = np.zeros((frame.particles.N,3)) #populate with random walks
-        frame.bonds.N = primitive.topology.number_of_edges()
-        frame.bonds.group = np.zeros((frame.bonds.N,2)) #populate this with bond indices
+        #frame.bonds.N = self.primitive.topology.number_of_edges()
+        #frame.bonds.group = np.zeros((frame.bonds.N,2)) #populate this with bond indices
+        bonds = []
         frame.bonds.types = ['a']
         L = np.cbrt(frame.particles.N / self.density) 
         if (L<3*self.r_cut):
             L = 3*self.r_cut
             print("Warning: Small number of particles, lowering density to {}, and L={}".format(frame.particles.N/(L**3),L))
         frame.configuration.box = [L, L, L, 0, 0, 0]
-        i = 0
-        bi = 0
-        for chain in primitive.topology.chains: 
+
+        h2i = {}
+        i=0
+        for chain in self.primitive.topology.chains:
+            head_handle, tail_handle = termini = self.get_termini_handles(chain) #TODO: Chains missing edges?
+            path : list[PrimitiveHandle] = next(all_simple_paths(chain, source=head_handle, target=tail_handle)) # raise StopIteration if no path exists
+            h2i[head_handle] = i
             frame.particles.position[i] = np.random.uniform(low=(-L/2),high=(L/2),size=3)
-            for bond in range(chain.number_of_edges()):
-                frame.bonds.group[bi+bond] = [i+bond,i+bond+1]
+            print("chain")
+            for prim_handle_outgoing, prim_handle_incoming in sliding_window(path, 2):
+                print("adding a bond")
+                i+=1
+                h2i[prim_handle_incoming] = i        
+                bonds.append( [h2i[prim_handle_outgoing],h2i[prim_handle_incoming]] )
                 delta = np.random.uniform(low=(-self.bond_l/2),high=(self.bond_l/2),size=3) #TODO
                 delta /= np.linalg.norm(delta)*self.bond_l
-                frame.particles.position[i+bond+1] = frame.particles.position[i+bond] + delta
-            i = i + chain.number_of_edges() + 1
-            bi = bi + chain.number_of_edges()
+                frame.particles.position[h2i[prim_handle_incoming]] = frame.particles.position[h2i[prim_handle_outgoing]] + delta
         frame.particles.position = pbc(frame.particles.position,[L,L,L])
+        frame.bonds.group = bonds
+        frame.bonds.N = len(frame.bonds.group)
         harmonic = hoomd.md.bond.Harmonic()
         harmonic.params["a"] = dict(r0=self.bond_l, k=self.k)
         integrator = hoomd.md.Integrator(dt=self.dt)
@@ -177,7 +186,9 @@ class DPD_RandomWalk(PlacementGenerator):
         while not check_inter_particle_distance(snap,minimum_distance=0.95): #TODO: update min_distance?
             simulation.run(1000)
         snap=simulation.state.get_snapshot()
-        return snap.particles.position
+
+        for s in snap.particles.position:
+            yield None, s
 
 
 
