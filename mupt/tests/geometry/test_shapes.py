47ead684fb941a43aadd355350ef0443095e40be
Timotej Bernat <tibe3324@colorado.edu>
2026-05-14 14:34:16 -0600

    Filled in unit test for BoundedShape.scale()

--- mupt/tests/geometry/test_shapes.py
+++ mupt/tests/geometry/test_shapes.py
@@ -43,15 +43,16 @@ def shapes_mixed() -> list[BoundedTransformableShape]:
     '''Combined collection of transformed and untransformed example shapes'''
     return [*shapes(), *shapes_transformed()]
 
-def shapes_with_volumes() -> list[tuple[BoundedTransformableShape, float]]:
+def shapes_with_volumes() -> list[tuple[BoundedTransformableShape, float]]: 
     '''Collection of shapes with known volumes, returned as (shape, expected volume) pairs'''
+    # DEV: only made function because multiple tests use these inputs
     return [
         (PointCloud.cubic(2.0), 8),
         (Cylinder(2, 4), 16*np.pi),
         (Sphere(3.0), 36*np.pi),
         (Ellipsoid.from_components(1, 2, 3), 8*np.pi),
     ]
-
+    
 
 # "Pure" shape tests
 @pytest.mark.parametrize('shape,volume_expected', shapes_with_volumes())
@@ -59,10 +60,43 @@ def test_volume(shape : BoundedShape, volume_expected : float) -> None:
     '''Test that volume calculation is accurate'''
     nptest.assert_allclose(shape.volume, volume_expected)
 
-@pytest.mark.parametrize('shape', shapes())
-def test_scaling(shape : BoundedShape) -> None:
-    # TODO: implement BoundedShape.__eq__() (and on subtypes) to permit easy comparisons
-    ...
+@pytest.mark.parametrize(
+    'shape,scaling_factor,shape_scaled_expected',
+    [
+        (PointCloud.cubic(1), 0.5, PointCloud.cubic(0.5)),
+        (PointCloud.cubic(1), 3.0, PointCloud.cubic(3.0)),
+        (Sphere(1), 0.5, Sphere(0.5)),
+        (Sphere(1), 2.5, Sphere(2.5)),
+        (Ellipsoid.from_components(1, 2, 3), 2.0, Ellipsoid.from_components(2, 4, 6)),
+        (
+            Ellipsoid(
+                radii=np.array([4, 5, 6]),
+                center=np.array([1., 2., 1.,])
+            ),
+            0.25, 
+            Ellipsoid(
+                radii=np.array([1.0, 1.25, 1.5]),
+                center=np.array([1., 2., 1.,])
+            ),
+        ),
+        (Cylinder(3.14, length=0.58), 0.75, Cylinder(2.355, length=0.435)),
+        (
+            Cylinder.from_radius_and_axis(3.6, np.array([1, 1, 1])),
+            2.0,
+            Cylinder.from_radius_and_axis(7.2, np.array([2, 2, 2])),
+        ),
+    ]
+)    
+def test_scaling(
+    shape : BoundedShape,
+    scaling_factor : float,
+    shape_scaled_expected : BoundedShape,
+) -> None:
+    '''Test that scaling operations give the expected shape of the same type'''
+    # scaled in-place, both to test that this also works AND because the BoundedShape
+    # base does not implement .scaled() (only BoundedTranformableShape does this)
+    shape.scale(scaling_factor) 
+    assert shape == shape_scaled_expected
 
 @pytest.mark.parametrize('shape,scaling_factor', cartesian(shapes(), (0.5, 1.0, 2.0)))
 def test_volume_scaling(shape : BoundedShape, scaling_factor : float) -> None:
@@ -81,7 +115,6 @@ def test_containment_centroidal(shape : BoundedShape) -> None:
 
 # Transformable shape tests
 @pytest.mark.parametrize('shape', shapes())
-# @pytest.mark.parametrize('shape', [])
 def test_equality(shape : BoundedTransformableShape) -> None:
     # NB: not checking direct equality with self, since that would be
     # true even if no __eq__ was implemented! (defaults to "is" behavior)
