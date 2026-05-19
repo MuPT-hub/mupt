1d74821e0dba4aad4c90eed6b7459c01bde1ac11
Timotej Bernat <tibe3324@colorado.edu>
2026-05-19 11:59:33 -0600

    Moved connector management Protocol and methods into .connections

--- mupt/mupr/connection/connectors.py
+++ mupt/mupr/connection/connectors.py
@@ -14,9 +14,14 @@ from typing import (
     Any,
     Callable,
     ClassVar,
+    Collection,
+    Generator,
     Hashable,
     Iterable,
+    Mapping,
     Optional,
+    Protocol,
+    TypeVar,
     TypeAlias,
 )
 
@@ -539,3 +544,36 @@ def canonical_form_connectors(connectors: Iterable[Connector], separator : str='
         separator=separator,
         joiner=joiner,
     )
+
+class ManagesConnectors(Protocol):
+    '''Interface for objects which manage Connectors and pairs of Connectors ("connections")'''
+    connectors : Collection[Connector]
+    connectors_by_address : Mapping[ConnectorAddress, Connector]
+    
+    def connector(self, conn_addr : ConnectorAddress) -> Connector:
+        ...
+
+    @property
+    def connectors_free(self) -> Collection[Connector]:
+        '''Connectors which are currently unbound'''
+        ...
+
+    @property
+    def connectors_bound(self) -> Collection[Connector]:
+        '''Connectors which have a neighbor'''
+        ...
+
+    # default implementations, for when explicitly inherited
+    @property
+    def functionality(self) -> int:
+        return len(self.connectors_free)
+    
+    @property
+    def valence(self) -> int: # DEV: well-defined from more than just atomic primitives since Connectors store BondType info
+        '''Electronic valence of the Primitive, i.e. the total bond order of all external-facing Connectors on this Primitive'''
+        total_bond_order : float = sum(
+            BOND_ORDER.get(conn.bondtype, 0.0)
+                for conn in self.connectors
+        )
+        return round(total_bond_order)
+    chemical_valence = electronic_valence = valence # aliases for convenience
