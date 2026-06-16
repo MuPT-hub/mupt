47f97761e0e92a219a58b64c5a855379642d2e79
timbernat <tibe3324@colorado.edu>
2026-06-15 22:19:25 -0600

    Deprecated ManagesConnector in favor of ConnectorManager

--- mupt/mupr/connection/__init__.py
+++ mupt/mupr/connection/__init__.py
@@ -1,13 +1,9 @@
 '''Abstractions of connections between structural units'''
 
-__author__ = 'Timotej Bernat'
-__email__ = 'timotej.bernat@colorado.edu'
-
 from .types import (
     AttachmentLabel,
     ConnectorLabel,
     ConnectorHandle,
-    ManagesConnectors,
 )
 from .exceptions import (
     ConnectionError,
