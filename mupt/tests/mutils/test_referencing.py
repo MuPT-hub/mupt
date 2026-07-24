db3b37355fa99e2c63f6a0a88ac70b0f7f753d90
timbernat <tibe3324@colorado.edu>
2026-07-23 19:35:35 -0600

    Outlined tests I'd like to have for Addressables (TODO: implement these)

--- mupt/tests/mutils/test_referencing.py
+++ mupt/tests/mutils/test_referencing.py
@@ -1,4 +1,23 @@
 '''Unit tests for object referencing'''
 
+from dataclasses import dataclass
+
 import pytest
-from mupt.mutils import referencing 
+from mupt.mutils.referencing import Addressable
+
+
+def test_has_address() -> None:
+    '''Test that Addressable objects indeed implement the address they claim to'''
+    ...
+
+def test_address_REgistration() -> None:
+    '''Test that newly-minted objects are also registered by their address in the classwide registry'''
+    ...
+
+def test_weak_address_refs() -> None:
+    '''Test that records of objects in classwide registry automatically vanish when object is garbage collected'''
+    ...
+
+def test_object_registries_distinct() -> None:
+    '''Test that distinct subtypes of Addressable do not share their classwide object registries'''
+    ...
