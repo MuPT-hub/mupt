fa1b118df7592fae3b61c0df4794e3d5cf875cac
timbernat <tibe3324@colorado.edu>
2026-07-27 11:24:02 -0600

    Added test for Addressable instances being assigned UUID and str address

--- mupt/tests/mutils/test_referencing.py
+++ mupt/tests/mutils/test_referencing.py
@@ -1,15 +1,42 @@
 '''Unit tests for object referencing'''
 
-from typing import Type
+from typing import Any, Type
 from dataclasses import dataclass
+from uuid import UUID
 
 import pytest
 from mupt.mutils.referencing import Addressable
 
 
-def test_has_address() -> None:
-    '''Test that Addressable objects indeed implement the address they claim to'''
+# dummy Addressable classes
+class DummyNoArgs(Addressable):
     ...
+
+class DummyWithArgs(Addressable):
+    def __init__(self, foo : str, bar : int=123) -> None:
+        self.foo = foo
+        self.bar = bar
+
+@dataclass # want to ensure addr registration mechanisms plays nice w/ dataclasses
+class DummyDataclass(Addressable):
+    baz : str
+    boo : float
+
+
+# tests proper
+@pytest.mark.parametrize(
+    'addr_typ,args',
+    [
+        (DummyNoArgs, {}),
+        (DummyWithArgs, {'foo' : 'abc', 'bar' : 42}),
+        (DummyDataclass, {'baz' : 'name', 'boo' : 3.14}),
+    ]
+)
+def test_has_address(addr_typ : Type[Addressable], args : dict[str, Any]) -> None:
+    '''Test that Addressable objects indeed implement the address they claim to'''
+    obj = addr_typ(**args)
+    assert hasattr(obj, 'address')
+    assert hasattr(obj, '_uuid') and isinstance(obj._uuid, UUID)
 
 def test_address_registration() -> None:
     '''Test that newly-minted objects are also registered by their address in the classwide registry'''
@@ -17,14 +44,14 @@ def test_address_registration() -> None:
 
 def test_weak_address_refs() -> None:
     '''Test that records of objects in classwide registry automatically vanish when object is garbage collected'''
-    class Dummy(Addressable):
-        ... # N.B.: defined locally to ensure reference counter to instances are not contaminated by other tests
+    class DummyLocal(Addressable):
+        ... # N.B.: defined locally to ensure reference counter to instances is not contaminated by other tests
 
-    obj = Dummy()
-    assert len(Dummy.registry_addresses) == 1
+    obj = DummyLocal()
+    assert len(DummyLocal.registry_addresses) == 1
 
     del obj 
-    assert len(Dummy.registry_addresses) == 0
+    assert len(DummyLocal.registry_addresses) == 0
 
 def test_object_registries_distinct() -> None:
     '''Test that distinct subtypes of Addressable do not share their classwide object registries'''
