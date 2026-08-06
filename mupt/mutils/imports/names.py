da10f6aa02c8fcdb1101ccfd010fee0d2d978deb
timbernat <tibe3324@colorado.edu>
2026-08-06 14:28:16 -0600

    Used resolve_module_name() to correct __spec__-related lint errors

--- mupt/mutils/imports/names.py
+++ mupt/mutils/imports/names.py
@@ -1,6 +1,6 @@
 '''For generating concise and convenient labels for ModuleLike objects'''
 
-from typing import Optional, Union
+from typing import Optional
 from types import ModuleType
 
 from importlib.resources._common import resolve
@@ -21,26 +21,29 @@ def resolve_module_name(module : Optional[ModuleType | str]) -> str:
 
     return importing_package_name
 
-def module_parts(module : Union[str, ModuleType]) -> tuple[Optional[str], str]:
+def module_parts(module : ModuleType | str) -> tuple[Optional[str], str]:
     '''Takes a module (as its name or as ModuleType) and returns its parent package name and relative module name'''
-    module = resolve(module)
-    module_name = module.__spec__.name
+    module_name : str = resolve_module_name(module)
     parent_package_name, _, module_stem = module_name.rpartition('.') # split on rightmost dot separator
     if not parent_package_name:
         parent_package_name = None
 
     return parent_package_name, module_stem
 
-def module_stem(module : Union[str, ModuleType]) -> str:
+def module_stem(module : ModuleType | str) -> str:
     '''Takes a module (as its name or as ModuleType) and returns its relative module name'''
     return module_parts(module)[-1]
 
-def relative_module_name(module : ModuleType, relative_to : Optional[ModuleType]=None, remove_leading_dot : bool=True) -> str:
+def relative_module_name(
+    module : ModuleType | str,
+    relative_to : Optional[ModuleType | str]=None,
+    remove_leading_dot : bool=True,
+) -> str:
     '''Gets the name of a module relative to another (presumably toplevel) module
     If the given module is not in the path of the toplevel module, will simply return as module.__name__'''
-    rel_mod_name = module.__spec__.name
+    rel_mod_name = resolve_module_name(module)
     if relative_to is not None:
-        toplevel_prefix = relative_to.__spec__.name
+        toplevel_prefix = resolve_module_name(relative_to)
         if remove_leading_dot:
             toplevel_prefix += '.' # append dot to prefix to remove it later
         rel_mod_name = rel_mod_name.removeprefix(toplevel_prefix)
