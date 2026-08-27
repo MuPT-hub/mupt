d6be15965b2a686d52a28ccca99d0482d60c31ce
Timotej Bernat <tibe3324@colorado.edu>
2026-08-26 18:46:42 -0600

    Updated method names to not use 'topology' spuriously

--- mupt/interfaces/_shared/traversal.py
+++ mupt/interfaces/_shared/traversal.py
@@ -5,8 +5,8 @@ from collections.abc import Hashable, Iterator, Mapping
 from dataclasses import dataclass, field
 
 from ...chemistry.core import BOND_ORDER
-from ...mupr import ConnectorReference
 from ...mupr.primitives import Primitive
+from ...mupr.linking import ConnectorReference
 from ...roles import PrimitiveRole
 
 
@@ -34,7 +34,7 @@ class SAAMRResidueRecord:
     particles: tuple[Primitive, ...]
 
 
-def build_saamr_role_topology_index(root: Primitive) -> SAAMRRoleTopologyIndex:
+def build_saamr_role_index(root: Primitive) -> SAAMRRoleTopologyIndex:
     """Build a single-pass role index for a SAAMR-like Primitive hierarchy.
 
     The accepted hierarchy is role based rather than depth based: UNASSIGNED
