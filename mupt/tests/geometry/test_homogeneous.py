6512924ca4cdb508ba54484714af59e90cc192ce
timbernat <tibe3324@colorado.edu>
2026-09-09 20:45:46 -0600

    Fixed F811 (variable override) and typing issues in test_homogeneous.py

--- mupt/tests/geometry/test_homogeneous.py
+++ mupt/tests/geometry/test_homogeneous.py
@@ -8,7 +8,7 @@ from mupt.geometry.transforms.affine.homogeneous import (
     to_homogeneous_coords,
     from_homogeneous_coords,
 )
-from mupt.geometry.arraytypes import Shape, N, Numeric
+from mupt.geometry.arraytypes import Vector3
 
 
 N: int = 10
@@ -25,9 +25,12 @@ block = np.random.random((N, N, 3))
         block,
     ),
 )
-def test_to_homogeneous_coords(array: np.ndarray[Shape[Any, 3], Numeric]) -> None:
-    """Test the conversion of arbitrarily-nested arrays of 3D coordinates to homogeneous coordinates"""
-    projection: int = 1.0
+def test_to_homogeneous_coords(array: Vector3) -> None:
+    """
+    Test the conversion of arbitrarily-nested arrays
+    of 3D coordinates to homogeneous coordinates
+    """
+    projection: float = 1.0
     *array_shape, xyz_shape = array.shape
     expected_shape = tuple(
         array_shape + [xyz_shape + 1]
@@ -45,7 +48,10 @@ def test_to_homogeneous_coords(array: np.ndarray[Shape[Any, 3], Numeric]) -> Non
         block,
     ),
 )
-def test_from_homogeneous_coords(array: np.ndarray[Shape[Any, 3], Numeric]) -> None:
-    """Test that conversion to and back from homogeneous coordinates has no side effects"""
+def test_from_homogeneous_coords(array: Vector3) -> None:
+    """
+    Test that conversion to and back from
+    homogeneous coordinates has no side effects
+    """
     piped_array = from_homogeneous_coords(to_homogeneous_coords(array))
     assert np.allclose(piped_array, array)
