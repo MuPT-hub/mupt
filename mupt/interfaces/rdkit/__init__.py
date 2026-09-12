7ae8b89f33301f0272161922c4e760fa520cdef4
timbernat <tibe3324@colorado.edu>
2026-09-11 23:25:14 -0600

    Removed bond_condition_by_atom_factory import (formatter makes too long, linter hates comma)

--- mupt/interfaces/rdkit/__init__.py
+++ mupt/interfaces/rdkit/__init__.py
@@ -18,7 +18,6 @@ from .selection import (
     all_bonds as all_bonds,
     no_bonds as no_bonds,
     bonds_by_condition as bonds_by_condition,
-    bond_condition_by_atom_condition_factory as bond_condition_by_atom_condition_factory
 )
 from .components import (
     chemical_graph_from_rdkit as chemical_graph_from_rdkit,
