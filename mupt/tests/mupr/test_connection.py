100e7689d519861fe5c64874fbdec9d9f65c9286
timbernat <tibe3324@colorado.edu>
2026-09-17 18:00:26 -0600

    Added unit tests from Connector fungibility

--- mupt/tests/mupr/test_connection.py
+++ mupt/tests/mupr/test_connection.py
@@ -2,12 +2,105 @@
 
 import pytest
 
+from networkx.algorithms import equivalence_classes
+
 from mupt.chemistry.core import BondType
 from mupt.mupr.connection import Connector, AttachmentPoint
 from mupt.builders.heading import TraversalDirection
 
 
-# bondability tests
+# Comparison tests
+CA = Connector(
+    anchor=AttachmentPoint({1}),
+    linker=AttachmentPoint({2}),
+    bondtype=BondType.DOUBLE,
+)
+## DEV: creating here since expected result must contain same literal Connector instance
+CA_COPY = CA.copy() 
+CA_COUNTERPART = CA.counterpart()
+
+# variant of CA with positional info; should compare as non-coincident
+CB = Connector(
+    anchor=AttachmentPoint({1}),
+    linker=AttachmentPoint({2}, position=[1,2,3]),
+    bondtype=BondType.DOUBLE,
+) 
+CB_COPY = CB.copy() 
+CB_COUNTERPART = CB.counterpart()
+
+CC = Connector(
+    anchor=AttachmentPoint({2}),
+    linker=AttachmentPoint({1, 3}),
+    bondtype=BondType.AROMATIC,
+)
+CC_COPY = CC.copy() 
+CC_COUNTERPART = CC.counterpart()
+
+CD = Connector(
+    anchor=AttachmentPoint({'same'}),
+    linker=AttachmentPoint({'same'}),
+    bondtype=BondType.SINGLE,
+)
+CD_COPY = CD.copy() 
+CD_COUNTERPART = CD.counterpart()
+
+
+@pytest.mark.parametrize(
+    'connectors,equiv_classes_expected',
+    [
+        ( # all 3 example prototype are non-fungible
+            (CA, CB, CC, CD),
+            {
+                frozenset([CA]),
+                frozenset([CB]),
+                frozenset([CC]),
+                frozenset([CD]),
+            }
+        ),
+        ( # test that copies are practially indistinguishable from originals
+            (
+                CA, CA_COPY,
+                CB, CB_COPY,
+                CC, CC_COPY,
+            ), 
+            {
+                frozenset([CA, CA_COPY]),
+                frozenset([CB, CB_COPY]),
+                frozenset([CC, CC_COPY]),
+            }
+        ),
+        ( # these Connectors are distinct from their counterparts
+            (CA, CA_COUNTERPART, CB, CB_COUNTERPART),
+            {
+                frozenset([CA]),
+                frozenset([CA_COUNTERPART]),
+                frozenset([CB]),
+                frozenset([CB_COUNTERPART]),
+            },
+        ),
+        ( # this Connector is actually designed to be equivalent to its counterpart
+            (CD, CD_COUNTERPART),
+            {frozenset([CD, CD_COUNTERPART])},
+        )
+    ],
+)
+def test_connector_fungibility(
+    connectors : tuple[Connector, ...],
+    equiv_classes_expected : set[frozenset[Connector]],
+) -> None:
+    """
+    Test that total comparison of Connectors for fungibility 
+    (i.e. interchangeability) groups together Connectors as expected
+    """
+    equiv_classes_actual = equivalence_classes(
+        connectors,
+        relation=Connector.fungible_with,
+    )
+    
+    assert equiv_classes_actual == equiv_classes_expected
+    
+
+# Bondability tests
 C1 = Connector(
     anchor=AttachmentPoint({"a"}),
     linker=AttachmentPoint({"z"}),
